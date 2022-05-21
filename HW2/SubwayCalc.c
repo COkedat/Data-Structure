5ed955e3522f1dcf539f506ef9f2081d77b8e1d2
COkedat <47716620+COkedat@users.noreply.github.com>
2022-05-21 18:11:33 +0900

    HW5 UPDATE
    틀짜기 시작

--- HW2/SubwayCalc.c
+++ HW2/SubwayCalc.c
@@ -1,5 +1,17 @@
-
+// 지하철 최단거리, 최소환승 계산기
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <random.h>
+struct element{ //구조체 함수
+    int data; //가는 시간
+    int ic; //환승 여부
+};
+
+
+
+
+
+int main(){
+    printf("pootis");
+    return 0;
+}
