a195f8a959991a1f7f0da0f38d723f718fafe60e
COkedat <47716620+COkedat@users.noreply.github.com>
2022-05-21 23:30:10 +0900

    HW5 업데이트
    대충 행렬 업데이트
    근데 KILL이랑 init 아직 작동안함

--- HW2/SubwayCalc.c
+++ HW2/SubwayCalc.c
@@ -3,17 +3,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+#define INF	9999
+
+int Sub_Time=0; // 이동 시간 전역변수 (환승 시간 제외)
+int IC_Time=0; // 환승 시간 전역변수
+int IC_Cnt=0; // 환승 횟수 전역변수
+int R=556; // 아마 행렬 전역변수
+int C=556; // 아마 행렬 전역변수
+int R_tmp=0; // 아마 행렬 전역변수
+int C_tmp=0; // 아마 행렬 전역변수
+
 typedef struct element{ // 구조체
-    char from; // 출발지
-    char to; // 목적지
+    //char* name; // 역이름?
+    char* from; // 출발지
+    char* to; // 목적지
     int data; // 가는 시간
     int ic; // 환승 여부
 } element;
 
-char *csvLists[20]={"1호선","2호선","3호선","4호선","5호선","6호선",
-                    "7호선","8호선","9호선","1호선","2호선","5호선",
-                    "경의선","경춘선","공항철도","분당선","인천1선","중앙선","역이름","환승정보"};
 
+//안쓸수도있음
+/*
+typedef struct GraphType {
+	int n;	// 정점의 개수
+	element** weight[][]; // 구조체 행렬
+} GraphType;
+
+//int distance[];
+//int found[];		
+*/
+
+char *csvLists[20]={"1호선","1지선","2호선","2지선","3호선","4호선",
+                    "5호선","5지선","6호선","7호선","8호선","9호선",
+                    "분당선","인천1선","중앙선","경춘선","경의선","공항철도","역이름","환승정보"};
+
+
+
+element** makeArray(){
+    int r,c=556;
+
+    element **arr = malloc(sizeof(element *) * r);   // 이중 포인터에 (element 포인터 크기 * row)만큼 동적 메모리 할당. 배열의 세로
+    for (int i = 0; i < r; i++)            // 세로 크기만큼 반복
+    {
+        arr[i] =malloc(sizeof(element) * c);    // (int의 크기 * col)만큼 동적 메모리 할당. 배열의 가로
+    }
+    return arr;
+}
+
+element** initArray(element** arr){
+    int r,c=556;
+
+    for (int i = 0; i < r; i++){
+        for (int j = 0; j < c; j++){
+                strcpy(arr[i][j].from,"");
+                strcpy(arr[i][j].to,"");
+                arr[i][j].data=9999;
+                arr[i][j].ic=FALSE;
+            }
+    }
+    return arr;
+}
+
+void killArray(element** arr){
+    int r=556;
+    printf("remove started\n");
+
+    for (int i = 0; i < r; i++) {
+        printf("remove started %d\n",i);
+        free(arr[i]);
+    }
+    free(arr);
+    printf("removed\n");
+}
+
+element makeElement(){
+
+}
 
 void readCSV(int i){
     char buf[2048];
@@ -39,10 +107,22 @@ void readCSV(int i){
 }
 
 
+
+//해야할거) 구조체 행렬 제작하기
+
+
 int main(){
     printf("pootis \n");
-    readCSV(1);
+    //readCSV(19);
+
+    element** test= makeArray();
+    test=initArray(test);
+    strcpy(test[0][0].from,"아싸좋아요");
+    printf("%s",test[0][0].from);
+    //printf("%d",test[0][0].data);
 
     //system("PAUSE");
+    killArray(test);
+    printf("sus\n");
     return 0;
 }
