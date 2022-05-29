1b71eed8b41b9acc6bf2ba4ae969ddf5d4936a3a
COkedat <47716620+COkedat@users.noreply.github.com>
2022-05-29 15:11:38 +0900

    HW2 업데이트
    대충 구실은 갖춤
    환승만 입력되면 될듯

--- HW2/SubwayCalc.c
+++ HW2/SubwayCalc.c
@@ -11,6 +11,7 @@
 #define MAX_VERTICES 552 //정점(역)의 수
 
 int Sub_Time=0; // 이동 시간 전역변수 (환승 시간 제외)
+int Sub_Cnt=0; // 정거장 횟수 전역변수 (환승 시간 제외)
 int IC_Time=0; // 환승 시간 전역변수
 int IC_Cnt=0; // 환승 횟수 전역변수
 const int R=MAX_VERTICES; // 행렬 상수
@@ -251,6 +252,40 @@ int subChk(sublist subinfo[],char chk[]){
     return -1;
 }
 
+void print_path(int start, int end,sublist subinfo[],element** arr){
+	/*
+	for (int q = 0; q < NODES; q++){
+		printf("%d : %d \n", q, path[q]);
+	}
+	*/
+
+	printf("<출발>\n");
+	int i = end;
+	int k = 0;
+	int way[R];
+	while (path[i] != -1){
+		way[k++]=i;
+		way[k++]=path[i];
+		i = path[i];
+	}
+    printf("-><%s> %s\n", csvLists[subinfo[way[k-1]].num],subinfo[way[k-1]].name);
+	for(int q = k - 1; q > 0; q=q-2){
+        if(arr[q][q-1].ic==0){
+            Sub_Time+=arr[q][q-1].data;
+            Sub_Cnt++;
+            printf("-><%s> %s\n", csvLists[subinfo[way[q-1]].num],subinfo[way[q-1]].name);
+        }
+        else{
+            IC_Time+=arr[q][q-1].data;
+            printf("-><환승 : 소요시간 %d 분> %s\n", arr[q][q-1].data,subinfo[way[q-1]].name);
+        }
+	}
+	printf("\n\n");
+    printf("소요시간 : %d (%d + 환승 소요시간 %d) 분\n",Sub_Time+IC_Time,Sub_Time,IC_Time);
+    printf("정거장 수 : %d 개\n",Sub_Cnt);
+}
+
+
 int main(){
     printf("프로그램 시작 \n");
     element** subarray= makeArray();
@@ -286,6 +321,8 @@ int main(){
     int ok=FALSE;
     char sub1[100];
     char sub2[100];
+    int sub1_idx;
+    int sub2_idx;
     while(ok!=TRUE){
         printf("출발역을 입력해주세요: ");
         fgets(sub1,sizeof(sub1),stdin); //출발역 입력
@@ -304,8 +341,8 @@ int main(){
             printf("다시 입력해주세요! \n");
             continue;
         }
-        int sub1_idx=subChk(subinfo,sub1);
-        int sub2_idx=subChk(subinfo,sub2);
+        sub1_idx=subChk(subinfo,sub1);
+        sub2_idx=subChk(subinfo,sub2);
         if (sub1_idx==-1||sub2_idx==-1){
             printf("출발역또는 도착역이 정확하지 않습니다! \n");
             printf("다시 입력해주세요! \n");
@@ -314,6 +351,9 @@ int main(){
         printf("[%s (%s) -> %s (%s)] \n", subinfo[sub1_idx].name,csvLists[subinfo[sub1_idx].num],subinfo[sub2_idx].name,csvLists[subinfo[sub2_idx].num]);
         ok=TRUE;
     }
+
+    shortest_path(subarray, sub1_idx);
+	print_path(sub1_idx, sub2_idx, subinfo,subarray);
 
     killArray(subarray);
     printf("프로그램 종료 \n");
